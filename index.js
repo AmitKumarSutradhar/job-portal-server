607e227c8b21b87da0139ccd925a0ff76c664e4c
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2024-12-14 23:59:51 +0600

    Job details api create

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,6 +28,22 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // Jobs related API
+    const jobCollection  = client.db('jobPortal').collection('jobs');
+    app.get('/jobs', async(req, res)=> {
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/jobs/:id', async(req, res)=>{
+      const jobId = req.params.id;
+      const query =  { _id : new ObjectId(jobId)};
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
